1e9207730d99486770c4220e1769739bf32f15dd
DongWon <ehddnjs1747@daum.net>
2023-01-31 16:38:35 +0900

    model view

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
 
 
 # Create your views here.
@@ -21,3 +21,9 @@ class LoginView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         token = serializer.validated_data
         return Response({"token": token.key}, status=status.HTTP_200_OK)
+
+
+
+class ProfileView(generics.GenericAPIView):
+    queryset = profile.objects.all()
+    serializer_class = ProfileSerializer
